1a2c43d3e75b34af56f09b57b35caeb238b3d8a6
prajavalpatki <prajavalpatki.com>
2022-03-07 20:40:12 +0530

    Calculate Daily Employee Wages

--- bridgelabz/EmployeeWage.java
+++ bridgelabz/EmployeeWage.java
@@ -4,13 +4,9 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
 
-        int IS_FULL_TIME = 1;
-        //computation
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME)
-            System.out.println("Employee is present");
-        else {
-            System.out.println("Employee is Absent");
-        }
+        int WAGE_PER_HR=20;
+        int DAILY_HR=8;
+        int EmployeeWage = (WAGE_PER_HR * DAILY_HR);
+        System.out.println("Daily Employee Wage= "+EmployeeWage);
     }
 }
