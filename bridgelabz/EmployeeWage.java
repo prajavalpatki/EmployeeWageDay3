38078c347149bc4b28d3dee0e1590ce79017f93d
prajavalpatki <prajavalpatki.com>
2022-03-07 18:02:51 +0530

    Employee present or absent

--- bridgelabz/EmployeeWage.java
+++ bridgelabz/EmployeeWage.java
@@ -3,5 +3,14 @@ package com.bl.bridgelabz;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage");
+
+        int IS_FULL_TIME = 1;
+        //computation
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is present");
+        else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
